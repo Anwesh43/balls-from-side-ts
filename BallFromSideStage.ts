9841a437f97098d012aff954f68d8e3f58eaba5e
Anwesh43 <anweshthecool0@gmail.com>
2019-04-13 16:05:29 +0530

    adding BallFromSideStage class

---
+++ BallFromSideStage.ts
@@ -0,0 +1,40 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const nodes : number = 5
+const balls : number = 4
+const foreColor : string = "#673AB7"
+const backColor : string = "#BDBDBD"
+
+class BallFromSideStage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = foreColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : BallFromSideStage = new BallFromSideStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
