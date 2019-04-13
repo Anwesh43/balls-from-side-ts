6727ced11f785804ad2572e176547b08c448a6d2
Anwesh43 <anweshthecool0@gmail.com>
2019-04-13 18:20:54 +0530

    adding BallFromSide class

--- BallFromSideStage.ts
+++ BallFromSideStage.ts
@@ -183,3 +183,26 @@ class BFSNode {
         return this
     }
 }
+
+class  BallFromSide {
+    root : BFSNode = new BFSNode(0)
+    curr : BFSNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
