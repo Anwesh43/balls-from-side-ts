e7f285d4de93c5b1f738cfc1a9ed6b069842311a
Anwesh43 <anweshthecool0@gmail.com>
2019-04-13 18:17:57 +0530

    adding BFSNode class

--- BallFromSideStage.ts
+++ BallFromSideStage.ts
@@ -139,3 +139,47 @@ class Animator {
         }
     }
 }
+
+class BFSNode {
+    prev : BFSNode
+    next : BFSNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new BFSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBFSNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BFSNode {
+        var curr : BFSNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
